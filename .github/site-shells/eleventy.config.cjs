60399911bb6304430011348b0cd67034fdec7b8e
Syasu-pixel <syasuta0819@gmail.com>
2026-09-18 17:31:23 +0900

    Pilot Eleventy shared header/footer regions with byte-preserving generation

---
+++ .github/site-shells/eleventy.config.cjs
@@ -0,0 +1,7 @@
+const { readFileSync } = require('node:fs');
+const { join } = require('node:path');
+module.exports = function(config) {
+  config.setNunjucksEnvironmentOptions({ throwOnUndefined: true, autoescape: true });
+  config.addGlobalData('shells', () => JSON.parse(readFileSync(join(__dirname, 'manifest.json'), 'utf8')).pages);
+  return { templateFormats: ['njk'], dir: { includes: 'components' } };
+};
